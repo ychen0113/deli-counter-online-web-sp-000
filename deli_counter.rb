ec3effac83edebb74f4e682c30c3410f2ff46a30
Yiliang Chen <yiliangchen0113@gmail.com>
2020-10-08 21:09:51 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
 # Write your code here.
+
+katz_deli = []
+
+def line(deli)
+  if deli.length == 0 
+    puts "The line is currently empty."
+  else 
+    x = []
+    x = deli.each_with_index {|a, index| puts "#{index + 1}. #{a}"}
+      puts "The line is currently: #{x.join}."
+  end
+end 
