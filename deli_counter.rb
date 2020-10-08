9cd211e45f2d54bbbb7e6f9208dad6bd3ce67ab0
Yiliang Chen <yiliangchen0113@gmail.com>
2020-10-08 21:21:31 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,7 +15,7 @@ end
 def take_a_number(deli, name)
   if deli.length == 0 
     deli << name 
-    puts "Welcome, #{name}. You are number #{deli.find_index}"
+    puts "Welcome, #{name}. You are number #{deli.index(name) + 1} in line."
     
   end
 end 
